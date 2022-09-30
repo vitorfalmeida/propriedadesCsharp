c0d5eaaa0f41b1e02473e5902e9999d30ad9584f
Vitor Fernandes <vitororama15@gmail.com>
2022-09-30 10:38:14 -0300

    Primeiro Save

---
+++ Model/Pessoa.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropMetCons.Model
+{
+    public class Pessoa
+    {
+        private String _nome;
+        private int _idade;
+        public String Nome
+        {
+            get => _nome.ToUpper();
+
+            set
+            {
+                if (value == "") throw new ArgumentException("O nome não pode ser vazio");
+                _nome = value;
+            }
+        }
+        public String Sobrenome { get; set; }
+        public String NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser negativa");
+                }
+                _idade = value;
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Meu nome é {NomeCompleto} e tenho {Idade} anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using PropMetCons.Model;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Vitor";
+p1.Sobrenome = "Fernandes";
+p1.Idade = 30;
+
+p1.Apresentar();
